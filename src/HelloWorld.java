f88efa8838713b72ec95349b30297cb58ead96f7
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-29 11:39:54 -0500

    first again again again

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("Hello World!");
+        System.out.print(" Goodbye World!");
+
+        int myFavoriteNumber = 4;
+        String myString = "Jenny loves dinosaurs!";
+
+    }
+}
