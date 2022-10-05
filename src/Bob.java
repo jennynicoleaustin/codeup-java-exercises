36a7a6c0fbd56820eefc61e41c160d09b0c73aa5
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-05 10:32:50 -0500

    string exercise done

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice = "y";
+        String askBob = "Sure";
+        String yellAtBob = "Whoa, chill out!";
+        String emptyAddress = "Fine. Be that way!";
+        String whatever = "Whatever.";
 
-    }
-}
+        System.out.println("Bob is in the room.");
+
+        System.out.println("Do you want to talk to Bob? (y/n): ");
+        choice = scanner.nextLine();
+
+        while (choice.equalsIgnoreCase("y")) {
+            System.out.println("Say something already!");
+            String addressBob = scanner.nextLine();
+
+            if (addressBob.endsWith("?")) {
+                System.out.println(askBob);
+            } else if (addressBob.endsWith("!")) {
+                System.out.println(yellAtBob);
+            } else if (addressBob.equals("")) {
+                System.out.println(emptyAddress);
+            } else {
+                System.out.println(whatever);
+            }
+            System.out.println("Do you have something else to say to Bob? (y/n): ");
+            choice = scanner.nextLine();
+
+        }
+    } //main close
+} // class close
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -51,57 +51,57 @@ public class ControlFlowExercises {
 //            }
 //        }
         // Table of Powers
-//        Scanner scanner = new Scanner(System.in);
-//        String choice = "y";
-//
-//        while (choice.equalsIgnoreCase("y")) {
-//            System.out.println("Enter an Integer: ");
-//            int userInput = scanner.nextInt();
-//
-//            System.out.println("Number" + "  " + "Squared" + "  " + "Cubed");
-//            System.out.println("======" + "  " + "======" + "  " + "======");
-//
-//            for (int i = 1; i <= userInput; i++) {
-//                int numberSquared = (int) Math.pow(i, 2);
-//                int numberCubed = (int) Math.pow(i, 3);
-//
-//                String message = "\n" + i + "       " + numberSquared + "       " + numberCubed;
-//
-//                System.out.println(message);
-//                System.out.println();
-//            }
-//                // see if the user wants to continue
-//                System.out.print("Continue? (y/n): ");
-//                choice = scanner.next();
-//                System.out.println();
-//            }
-
-        //Number grades to letter grades
         Scanner scanner = new Scanner(System.in);
         String choice = "y";
 
         while (choice.equalsIgnoreCase("y")) {
-            System.out.println("Enter your grade: ");
-            int numGrade = scanner.nextInt();
-            scanner.nextLine();
+            System.out.println("Enter an Integer: ");
+            int userInput = scanner.nextInt();
 
-            if (numGrade <= 100 && numGrade >= 88) {
-                System.out.println("A");
-            } else if (numGrade <= 87 && numGrade >= 80) {
-                System.out.println("B");
-            } else if (numGrade <= 79 && numGrade >= 67) {
-                System.out.println("C");
-            } else if (numGrade <= 66 && numGrade >= 60) {
-                System.out.println("D");
-            } else if (numGrade <= 59 && numGrade >= 0) {
-                System.out.println("F");
-            } else {
-                System.out.println("Enter a value from 0 to 100");
+            System.out.println("Number" + "  " + "Squared" + "  " + "Cubed");
+            System.out.println("======" + "  " + "======" + "  " + "======");
+
+            for (int i = 1; i <= userInput; i++) {
+                int numberSquared = (int) Math.pow(i, 2);
+                int numberCubed = (int) Math.pow(i, 3);
+
+                String message = "\n" + i + "       " + numberSquared + "       " + numberCubed;
+
+                System.out.println(message);
+                System.out.println();
             }
-            System.out.println("Do you wish to continue?");
-            choice = scanner.next();
-            scanner.nextLine();
-        }
+                // see if the user wants to continue
+                System.out.print("Continue? (y/n): ");
+                choice = scanner.next();
+                System.out.println();
+            }
+
+        //Number grades to letter grades
+//        Scanner scanner = new Scanner(System.in);
+//        String choice = "y";
+//
+//        while (choice.equalsIgnoreCase("y")) {
+//            System.out.println("Enter your grade: ");
+//            int numGrade = scanner.nextInt();
+//            scanner.nextLine();
+//
+//            if (numGrade <= 100 && numGrade >= 88) {
+//                System.out.println("A");
+//            } else if (numGrade <= 87 && numGrade >= 80) {
+//                System.out.println("B");
+//            } else if (numGrade <= 79 && numGrade >= 67) {
+//                System.out.println("C");
+//            } else if (numGrade <= 66 && numGrade >= 60) {
+//                System.out.println("D");
+//            } else if (numGrade <= 59 && numGrade >= 0) {
+//                System.out.println("F");
+//            } else {
+//                System.out.println("Enter a value from 0 to 100");
+//            }
+//            System.out.println("Do you wish to continue?");
+//            choice = scanner.next();
+//            scanner.nextLine();
+//        }
 
     } // main close
 } //public class close
