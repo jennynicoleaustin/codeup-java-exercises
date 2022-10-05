0400df69075961517eb5ed1d5b2f966ec5e1993c
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-05 12:14:12 -0500

    Methods getInt done

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class MethodExercises {
     public static void main(String[] args) {
         addition(3, 4);
         subtraction(6, 4);
-        multiplication(2,3);
+        multiplication(2, 3);
         divide(12, 2);
         remainder(6, 2);
+
+        getInteger(1, 10);
 
     } //main method close
 
@@ -32,11 +36,24 @@ public class MethodExercises {
         return res;
     } // divide method close
 
-     public static int remainder(int x, int y) {
+    public static int remainder(int x, int y) {
         int res = x % y;
         System.out.println(res);
         return res;
     } // modulus method close
 
+    public static int getInteger(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
 
-} // class close
+        System.out.println("Enter a number between 1 and 10: ");
+        int userInput = scanner.nextInt();
+
+        if (userInput < 1 || userInput > 10) {
+            getInteger(min, max);
+        } else {
+            System.out.println(userInput);
+        }
+        return userInput;
+    } // getInteger method close
+
+} // MethodsExercises class close
