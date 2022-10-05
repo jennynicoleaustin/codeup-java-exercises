a82d53034fb29ba652d87e68b85921f8600f2b67
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-05 08:12:48 -0500

    strings pt 1

---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String education = "We don't need no education\nWe don't need no thought control";
+        System.out.println(education);
+
+        String checkItOut = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkItOut);
+
+        String backslashes = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(backslashes);
+
+    } //main close
+} //class close
