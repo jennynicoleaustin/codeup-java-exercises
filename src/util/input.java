cdfd857e89eb0d6fc451af8295d20b4d89ca741d
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-06 11:21:26 -0500

    OOP exercises

---
+++ src/util/input.java
@@ -0,0 +1,56 @@
+package util;
+
+import java.util.Scanner;
+
+public class input {
+    private Scanner sc;
+
+    public input() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return this.sc.nextLine();
+    }
+
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return getString();
+    }
+
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        String input = this.sc.next();
+        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+    }
+
+    public int getInt(int min, int max, String prompt) {
+        int input;
+        do {
+            System.out.println(prompt);
+            input = this.sc.nextInt();
+        } while (input < min || input > max);
+        return input;
+    }
+
+    public int getInt() {
+        System.out.println("Enter an integer:");
+        return this.sc.nextInt();
+
+    }
+
+    public double getDouble() {
+        System.out.println("Enter an really big integer:");
+        return this.sc.nextDouble();
+    }
+
+    public double getDouble(double min, double max, String prompt) {
+        System.out.println(prompt);
+        double input;
+        do {
+            System.out.println(prompt);
+            input = this.sc.nextDouble();
+        } while (input < min || input > max);
+        return input;
+    }
+}
